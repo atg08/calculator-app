d38c49081125e37907fd5ebeddf23cb805d8c5e4
AdittiGupta <agup0031@student.monash.edu>
2023-09-08 00:00:17 +0800

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,131 @@
+<script lang="ts">
+    const numbers = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "0" , "."];
+    const operations = ["/" , "x" , "+" , "-" , "="];
+
+    let selecterOperation = "";
+    let display = ""
+    let firstNumber = "";
+    let secondNumber = "";
+
+    const handleOperationClick = (operation: string) => {
+        selecterOperation = operation;
+    };
+
+    const handleNumberClick = (number: string) => {
+        if(display === "" && number === "0") return;
+        if (number === "." && display.includes('.')) return;
+        if(display === "" && number === ".") return (display = "0.");
+
+        if(!selecterOperation){
+            if(display === "" && number === ".") {
+            firstNumber = "0."
+            return display = firstNumber
+            }
+            firstNumber = `${firstNumber}${number}`
+            return display = firstNumber;
+
+        } else {
+            if(display === "" && number === ".") {
+            secondNumber = "0."
+            return display = secondNumber
+            }
+            secondNumber = `${secondNumber}${number}`
+            return display = secondNumber;
+
+        }
+
+    }
+
+
+</script>
+
+
+
+<main>
+    <div class = 'calculator'>
+        <div class="results">
+            {display}
+        </div>
+        <div class="digits">
+            <div class="numbers">
+                <button class = "btn btn-xlg"> C </button>
+                
+                {#each numbers as number (number)}
+                    <button class={`btn ${number === "0" ? "btn-lg" : null}`} on:click={() => handleNumberClick(number)}> 
+                        {number} </button>       
+                {/each}
+
+            </div>     
+            <div class="operations">
+                {#each operations as operation (operation)}
+                    <button class={`btn ${operation === selecterOperation ? "btn-silver" : "btn-rown"}`} 
+                        on:click={() => handleOperationClick(operation)}> {operation} </button>       
+                {/each}
+            </div>       
+        </div>
+    </div>
+</main>
+
+
+
+<style>
+    main {
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .calculator {
+      background-color: rgb(191, 170, 140); 
+      width: 240px;
+      padding: 15px;
+      border-radius: 7px;
+    }
+    .digits {
+      display: flex;
+    }
+    .operations {
+      display: flex;
+      flex-direction: column;
+    }
+    .numbers {
+      display: flex;
+      flex-wrap: wrap;
+      width: 200px;
+    }
+    .btn {
+      width: 50px;
+      height: 50px;
+      border-radius: 100px;
+      background-color: rgb(68 , 41 , 22); 
+      font-size: 20px;
+      font-weight: bold;
+      color: white;
+      margin: 5px;
+      border: none;
+    }
+    .btn-lg {
+      width: 110px;
+    }
+    .btn-brown {
+      background-color: rgb(44, 29, 0);
+    }
+    .btn-silver {
+      background-color: rgb(152, 117, 84);
+    }
+    .btn-xlg {
+      width: 170px;
+    }
+    .results {
+      height: 60px;
+      color: white;
+      font-size: 50px;
+      display: flex;
+      flex-direction: row-reverse;
+      margin-right: 10px;
+    }
+  </style>
+`
+
+
